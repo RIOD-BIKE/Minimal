426ea92f0459961dfa0808c94f5c244767078f46
Duc Thang Duong <thangduong1611@gmail.com>
2020-06-05 14:23:22 +0200

    Timer Animation

--- src/app/Components/waiting-at-as-small/waiting-at-as-small.component.ts
+++ src/app/Components/waiting-at-as-small/waiting-at-as-small.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MapBoxComponent } from '../map-box/map-box.component';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ModalController } from '@ionic/angular';
 
 
 @Component({
@@ -7,18 +10,40 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./waiting-at-as-small.component.scss'],
 })
 export class WaitingAtAsSmallComponent implements OnInit {
-	progessCircleSize = 200; //change the size and radius of Circle Progess hier, value depend on Time. Max =200 Min=
-	progessSize: any = 100; //change the width of progess bar hier, value depend on Time. Max =100
-	progessWidth = this.progessSize + '%';
+	progessCircleSize = 180; //change the size and radius of Circle Progess hier, value depend on Time. Max =180 Min=100
+	progessSize = 100; //change the width of progess bar hier, value depend on Time. Max =100
+	progessGo: any = false;
 	progessCircleRadius = (this.progessCircleSize / 2) + 'px';
 	progessColor = 'linear-gradient(to right,red, #383838, red)';
-	progessGo: any = true;
 	progessCircleColor = 'radial-gradient(circle, #383838,#383838, red)';
+	/*Timer*/
+	public second = 59;
+	public minute = 2;
+	IntervalVar: any;
 
-	ngOnInit() {
-
+	constructor(private mapBox: MapBoxComponent, private statusBar: StatusBar, private modalController: ModalController) { }
+	startTimer() {
+		this.IntervalVar = setInterval(function () {
+			this.second--;
+			this.progessCircleSize = this.progessCircleSize - 0.7;
+			this.progessSize = this.progessSize - (100/180);
+			if (this.second === 0) {
+				this.second = 59;
+				this.minute -= 1;
+			}
+			if (this.minute < 0) {
+				this.second = '30';
+				this.minute = '00';
+				clearInterval(this.IntervalVar);
+				this.progessGo = true;
+				this.progessCircleSize = 180; //change the size and radius of Circle Progess hier, value depend on Time. Max =180 Min=100
+				this.progessSize = 100; //change the width of progess bar hier, value depend on Time. Max =100
+	
+				// this.hidevalue = true;
+			}
 		var circleProgess = document.getElementById("progessCircle");
-		var timerCircle = document.getElementById("timerCircle");
+		var progessBar = document.getElementById("progessBar");
+		progessBar.style.width = this.progessSize + '%';
 		circleProgess.style.width = this.progessCircleSize + 'px';
 		circleProgess.style.height = this.progessCircleSize + 'px';
 		circleProgess.style.borderRadius = this.progessCircleRadius;
@@ -29,12 +54,22 @@ export class WaitingAtAsSmallComponent implements OnInit {
 		if (this.progessGo) {
 			this.progessColor = 'linear-gradient(to right,green, #383838, green)';
 		}
-		if (this.progessCircleSize < 141 && this.progessGo == false) {
+		if (this.progessCircleSize < 92 && this.progessGo == false) {
 			this.progessCircleColor = 'radial-gradient(circle, #383838,#383838, yellow)';
 		}
 		if (this.progessGo) {
 			this.progessCircleColor = 'radial-gradient(circle, #383838,#383838, green)';
 		}
+		}.bind(this), 1000);
+	}
+
+	ngOnInit() {
+
+		
+
+		/* Timer */
+
+		this.startTimer();
 	}
 	showMore() {
 		var changeDivHeight = document.getElementById("myOver");
@@ -64,6 +99,4 @@ export class WaitingAtAsSmallComponent implements OnInit {
 		y.hidden = false;
 		timerCircle.hidden = true;
 	}
-
-
 }
